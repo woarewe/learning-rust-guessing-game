803db0474c780cf9dc02040c2cc641a928b53553
woarewe <rostislav.zhuravsky@gmail.com>
2023-01-09 03:21:00 +0100

    Generate random number'

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
@@ -6,9 +7,12 @@ fn main() {
 
     let mut guess = String::new();
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
 
     println!("Your guess: {guess}");
+    println!("Secret number is: {secret_number}");
 }
