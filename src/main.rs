b1bb2705b403dcba253277b67c9fa976038f3d8b
woarewe <rostislav.zhuravsky@gmail.com>
2023-01-09 03:33:30 +0100

    Finish the game when a win

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,32 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline");
-
-    let guess = guess.trim().parse::<u32>().expect("Please type a number");
     
-    println!("Your guess: {guess}");
+    
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        let mut guess = String::new();
+
+        println!("Please input your guess.");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
+
+        let guess = guess.trim().parse::<u32>().expect("Please type a number");
+
+        println!("Your guess: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
