62a51665475881cbaa956732057bb99cc71b3cc3
woarewe <rostislav.zhuravsky@gmail.com>
2023-01-22 11:49:44 +0100

    Split module into separate files

--- restaraunt/src/lib.rs
+++ restaraunt/src/lib.rs
@@ -1,49 +1,7 @@
-mod front_of_house {
-    pub struct Order {
-        id: usize
-    }
+mod front_of_house;
 
-    impl Order {
-        pub fn new(id: usize) -> Self {
-            Self { id }
-        }
-    }
+pub use crate::front_of_house::hosting;
 
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        pub fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-
-        pub fn server_order() {}
-
-        pub fn take_payment() {}
-    }
-}
-
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    front_of_house::hosting::add_to_waitlist();
-
-    let a: front_of_house::Order;
-    // The below line won't compile and I am so happy about that
-    // let b = front_of_house::Order { id: 34 }; 
-    a = front_of_house::Order::new(34);
-}
-
-fn deliver_order() {}
-
-mod back_of_house {
-    fn fix_incorrect_order() {
-        use super::*;
-
-        cook_order();
-        deliver_order();
-    }
-
-    fn cook_order() {}
+pub fn eat_at_restaraunt() {
+    hosting::add_to_waitlist();
 }
