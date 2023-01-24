f4b5f8bca1d766cc2d6a29997d73a266e2986bbe
woarewe <rostislav.zhuravsky@gmail.com>
2023-01-24 21:18:52 +0100

    Handle different scenario

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 
 fn main() {
-    let result = File::open("../tmp/helsflo.txt");
-
+    let path = "../tmp/helsflo.txt";
+    let result = File::open(path);
 
     let file = match result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Creating file failed {:?}", e)
+            },
+            other_error => {
+                panic!("Problem with opening file {:?}", other_error);
+            }
+        }
     };
 }
