10365aad5f0166dea9ce8b71bb417f398189e5ad
woarewe <rostislav.zhuravsky@gmail.com>
2023-01-30 22:29:50 +0100

    The largest with a function

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,13 +1,18 @@
-fn main() {
-   let number_list = vec![34, 50, 25, 101, 60];
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-   let mut largest = &number_list[0];
-
-   for number in &number_list {
+    for number in list {
         if number > largest {
             largest = number;
         }
-   }
+    }
 
-   println!("The largest number is {}", largest);
+    largest
+}
+
+
+fn main() {
+   let number_list = vec![34, 50, 25, 101, 60];
+
+   println!("The largest number is {}", largest(&number_list));
 }
