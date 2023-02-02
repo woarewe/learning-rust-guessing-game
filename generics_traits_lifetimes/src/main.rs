8ae04dfd79f2efef2a871fe76a564634735e2bd6
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-02 18:28:32 +0100

    Implement generic function

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for number in list {
@@ -15,4 +15,7 @@ fn main() {
    let number_list = vec![34, 50, 25, 101, 60];
 
    println!("The largest number is {}", largest(&number_list));
+
+   let char_list = vec!['y', 'm', 'a', 'q'];
+   println!("The largest char is {}", largest(&char_list));
 }
