69698315eccb6f2fdb43c86dfe750f374b3a3603
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-02 18:40:15 +0100

    Add generics to methods and impl for specific types

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -10,6 +10,23 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     largest
 }
 
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U
+}
+
+impl  <T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<i32, i32> {
+    fn int_x(&self) -> &i32 {
+        &self.x
+    }
+}
 
 fn main() {
    let number_list = vec![34, 50, 25, 101, 60];
@@ -18,4 +35,17 @@ fn main() {
 
    let char_list = vec!['y', 'm', 'a', 'q'];
    println!("The largest char is {}", largest(&char_list));
+
+   let both_integers = Point { x: 5, y: 10 };
+   let both_float = Point { x: 1.2, y: 3.0 };
+   let integer_and_float = Point { x: 5, y: 6.0 };
+
+   println!("{:?} {:?} {:?}", both_integers, both_float, integer_and_float);
+
+   println!("{:?}", both_integers.x());
+   println!("{:?}", both_integers.int_x());
+
+   // Won't compile
+   // println!("{:?}", integer_and_float.int_x());
+
 }
