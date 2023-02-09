209117d89fba86efb456dde8a2558bf0ed76abf6
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-09 22:58:31 +0100

    Finish the lifitime chapter

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -17,10 +17,41 @@ impl Len for String {
     }
 }
 
+impl Len for str {
+    fn len(&self) -> usize {
+        self.len()
+    }
+}
+
+fn longestv2<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn new_str<'a>(first: &str, second: &str) -> String {
+    // Won't work as well cause the value is owned by a function
+    String::from("really long string")
+}
+
 fn main() {
     let a = String::from("short");
     let b = String::from("long long");
 
 
     println!("The longest is: {}", longest(&a, &b));
+
+    let string1 = String::from("long string is long");
+    let result;
+
+    {
+        let string2 = String::from("short");
+        result =  longestv2(string1.as_str(), string2.as_str());
+        // Works just fine
+        println!("The longest is {}", result);
+    }
+    // Won't be compiled
+    // println!("The longest is {}", result);
 }
