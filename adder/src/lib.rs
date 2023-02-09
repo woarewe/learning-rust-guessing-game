d03ab35553250d7cf26e872a545d96555040d8c0
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-09 23:40:46 +0100

    Add expected message to a should panic attribute

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -28,8 +28,11 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        }
+        if value < 1 {
+            panic!("Guess value must be greather than or equal to 1, got {}.", value);
         }
 
         Guess { value }
@@ -91,7 +94,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
