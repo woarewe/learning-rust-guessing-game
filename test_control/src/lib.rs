f31dd25c29e44361ea35f8ad3812d9f4f1ad09ce
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-11 18:14:22 +0100

    Learn tests execution controll

--- test_control/src/lib.rs
+++ test_control/src/lib.rs
@@ -3,19 +3,39 @@ fn prints_and_returns_10(value: i32) -> i32 {
     10
 }
 
+fn add_two(value: i32) -> i32 {
+    value + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn this_test_will_pass() {
-        let value = prints_and_returns_10(8);
+        let value = prints_and_returns_10(4);
         assert_eq!(value, 10);
     }
 
     #[test]
+    #[ignore]
     fn this_test_will_fail() {
         let value = prints_and_returns_10(8);
         assert_eq!(value, 8);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
