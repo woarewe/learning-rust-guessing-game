b68102d7bc1d1846a4b18667742298930f76eb9a
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-11 18:04:44 +0100

    Only failed tests can print to stdout

---
+++ test_control/src/lib.rs
@@ -0,0 +1,21 @@
+fn prints_and_returns_10(value: i32) -> i32 {
+    println!("I got the value {}", value);
+    10
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 10);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(value, 8);
+    }
+}
