be22b03af1dd094bedcffda040b98830be4c0d69
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-11 22:03:31 +0100

    Move CLI args parsing to a separate function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 use std::fs;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    (query, file_path)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (query, file_path) = parse_config(&args);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
