edc513d98c3d41cbf24603a420abc158e79d322d
woarewe <rostislav.zhuravsky@gmail.com>
2023-02-14 23:10:23 +0100

    Create a lib crate and move the logic there

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,41 +1,15 @@
-use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Self { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
