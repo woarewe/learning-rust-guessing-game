ac93b554706e198711b6fb75665989435824bd59
woarewe <rostislav.zhuravsky@gmail.com>
2023-03-14 22:45:47 +0100

    Finish smart pointer

--- smart_points/src/main.rs
+++ smart_points/src/main.rs
@@ -1,3 +1,16 @@
+#[derive(Debug)]
+enum List<T> {
+    Cons(T, Box<List<T>>),
+    Nil
+}
+
+impl<T> List<T> {
+    pub fn cons(value: T, next: List<T>) -> List<T> {
+        Self::Cons(value, Box::new(next))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let list = List::cons(3, List::cons(2, List::cons(1, List::Nil)));
+    println!("{:?}", list);
 }
